e0dbe1a3300e83169d561b3ff5e94e9d91aa8700
Simon-157 <boatengsimonjnr157@gmail.com>
2022-11-17 19:41:57 +0000

    ui revamp

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@material-ui/core";
 import "../App.css";
+import Navbar from "./Navbar";
 
 function Home() {
   const navigate = useNavigate();
@@ -9,33 +10,36 @@ function Home() {
     navigate("/predict");
   };
   return (
-    <div className="homecontainer">
-      <div className="banner">
-        <div className="banner__title">
-          <div className="banner__title_head">
-            OnlineShopping<font>AI</font>
-          </div>
-          <div className="banner__title_tail">
-            <div className="typing">
-              A Machine Learning based Web Application for Predicting the intent
-              of a customer making a <br />
-              <strong>
-                purchase from an online shop within a browsing session using the
-                user's cookies information
-              </strong>
+    <>
+      <Navbar />
+      <div className="homecontainer">
+        <div className="banner">
+          <div className="banner__title">
+            <div className="banner__title_head">
+              OnlineShopping<font>AI</font>
             </div>
-            <div className="banner__buttons">
-              <Button
-                onClick={predictRedirect}
-                className="banner__button cropButton"
-              >
-                Start Prediction
-              </Button>
+            <div className="banner__title_tail">
+              <div className="typing">
+                A Machine Learning based Web Application for Predicting the
+                intent of a customer making a <br />
+                <strong>
+                  purchase from an online shop within a browsing session using
+                  the user's cookies information
+                </strong>
+              </div>
+              <div className="banner__buttons">
+                <Button
+                  onClick={predictRedirect}
+                  className="banner__button cropButton"
+                >
+                  Start Prediction
+                </Button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- client/src/components/PredictIntent.js
+++ client/src/components/PredictIntent.js
@@ -17,7 +17,8 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import Loading from "./Loading";
-
+import "../App.css";
+import Navbar from "./Navbar";
 const useStyles = makeStyles({
   root: {
     maxWidth: 550,
@@ -87,22 +88,22 @@ function PredictIntent() {
           "Customer will not make a purchase" ? (
             <CardMedia
               component="img"
-              height="194"
-              image="/assets/dis.png"
+              height="200"
+              src="https://assets.entrepreneur.com/content/3x2/2000/20151125105751-questions-1014060-1920.jpeg?crop=1:1"
               alt="not purchased"
             />
           ) : (
             <CardMedia
               component="img"
               height="194"
-              image="/assets/well.png"
+              image="https://ownrblog.com/wp-content/uploads/2020/06/purchase-order-defintion.jpg"
               alt="not purchased"
             />
           )}
 
           <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              <b>Prediction: </b>
+            <Typography gutterBottom align="center" variant="h5" component="h2">
+              <b>Prediction for customer's intent </b>
             </Typography>
             <Typography
               variant="body2"
@@ -141,7 +142,7 @@ function PredictIntent() {
               size="small"
               color="primary"
             >
-              Back to Prediction
+              Back to Predict
             </Button>
           </CardActions>
         </Card>
@@ -153,175 +154,178 @@ function PredictIntent() {
     return <Loading />;
   } else
     return (
-      <div className="form">
-        <div className="form__form_group">
-          {predictionData.error && (
-            <>
-              <Alert
-                onClose={() => {}}
-                style={{ marginTop: "20px" }}
-                severity="error"
-              >
-                {" "}
-                {predictionData.error}{" "}
-              </Alert>
-            </>
-          )}
+      <>
+        <Navbar />
+        <div className="form">
+          <div className="form__form_group">
+            {predictionData.error && (
+              <>
+                <Alert
+                  onClose={() => {}}
+                  style={{ marginTop: "20px" }}
+                  severity="error"
+                >
+                  {" "}
+                  {predictionData.error}{" "}
+                </Alert>
+              </>
+            )}
 
-          <center>
-            <div className="form__title">
-              Customer Purchase Intent Prediction
-            </div>
-          </center>
-          <TextField
-            onChange={(e) => handleChange(e)}
-            value={formData.Administrative}
-            className="form__text_field"
-            id="Administrative"
-            name="Administrative"
-            variant="filled"
-            label="Administrative"
-          />
-          <TextField
-            onChange={(e) => handleChange(e)}
-            value={formData.Administrative_Duration}
-            className="form__text_field"
-            id="Administrative_Duration"
-            name="Administrative_Duration"
-            variant="filled"
-            label="Administrative_Duration"
-          />
-          <TextField
-            onChange={(e) => handleChange(e)}
-            value={formData.Informational}
-            className="form__text_field"
-            id="Informational"
-            name="Informational"
-            variant="filled"
-            label="Informational"
-          />
-          <TextField
-            onChange={(e) => handleChange(e)}
-            value={formData.Informational_Duration}
-            className="form__text_field"
-            id="Informational_Duration"
-            name="Informational_Duration"
-            variant="filled"
-            label="Informational_Duration"
-          />
+            <center>
+              <div className="form__title">
+                Customer Purchase Intent Prediction
+              </div>
+            </center>
+            <TextField
+              onChange={(e) => handleChange(e)}
+              value={formData.Administrative}
+              className="form__text_field"
+              id="Administrative"
+              name="Administrative"
+              variant="filled"
+              label="Administrative"
+            />
+            <TextField
+              onChange={(e) => handleChange(e)}
+              value={formData.Administrative_Duration}
+              className="form__text_field"
+              id="Administrative_Duration"
+              name="Administrative_Duration"
+              variant="filled"
+              label="Administrative_Duration"
+            />
+            <TextField
+              onChange={(e) => handleChange(e)}
+              value={formData.Informational}
+              className="form__text_field"
+              id="Informational"
+              name="Informational"
+              variant="filled"
+              label="Informational"
+            />
+            <TextField
+              onChange={(e) => handleChange(e)}
+              value={formData.Informational_Duration}
+              className="form__text_field"
+              id="Informational_Duration"
+              name="Informational_Duration"
+              variant="filled"
+              label="Informational_Duration"
+            />
 
-          <TextField
-            onChange={(e) => handleChange(e)}
-            value={formData.ProductRelated}
-            className="form__text_field"
-            id="ProductRelated"
-            name="ProductRelated"
-            variant="filled"
-            label="ProductRelated"
-          />
+            <TextField
+              onChange={(e) => handleChange(e)}
+              value={formData.ProductRelated}
+              className="form__text_field"
+              id="ProductRelated"
+              name="ProductRelated"
+              variant="filled"
+              label="ProductRelated"
+            />
 
-          <TextField
-            onChange={(e) => handleChange(e)}
-            value={formData.ProductRelated_Duration}
-            className="form__text_field"
-            id="ProductRelated_Duration"
-            name="ProductRelated_Duration"
-            variant="filled"
-            label="ProductRelated_Duration"
-          />
+            <TextField
+              onChange={(e) => handleChange(e)}
+              value={formData.ProductRelated_Duration}
+              className="form__text_field"
+              id="ProductRelated_Duration"
+              name="ProductRelated_Duration"
+              variant="filled"
+              label="ProductRelated_Duration"
+            />
 
-          <TextField
-            onChange={(e) => handleChange(e)}
-            value={formData.BounceRates}
-            className="form__text_field"
-            id="BounceRates"
-            name="BounceRates"
-            variant="filled"
-            label="BounceRates(float)"
-          />
-          <TextField
-            onChange={(e) => handleChange(e)}
-            value={formData.ExitRates}
-            className="form__text_field"
-            id="ExitRates"
-            name="ExitRates"
-            variant="filled"
-            label="ExitRates(float)"
-          />
-          <TextField
-            onChange={(e) => handleChange(e)}
-            value={formData.PageValues}
-            className="form__text_field"
-            id="PageValues"
-            name="PageValues"
-            variant="filled"
-            label="PageValues (float)"
-          />
-          <TextField
-            onChange={(e) => handleChange(e)}
-            value={formData.SpecialDay}
-            className="form__text_field"
-            id="SpecialDay"
-            name="SpecialDay"
-            variant="filled"
-            label="SpecialDay (float)"
-          />
-          <TextField
-            onChange={(e) => handleChange(e)}
-            value={formData.Month}
-            className="form__text_field"
-            id="Month"
-            name="Month"
-            variant="filled"
-            label="Month in numbers starting from 0"
-          />
-          <TextField
-            onChange={(e) => handleChange(e)}
-            value={formData.OperatingSystems}
-            className="form__text_field"
-            id="OperatingSystems"
-            name="OperatingSystems"
-            variant="filled"
-            label="OperatingSystems"
-          />
-          <TextField
-            onChange={(e) => handleChange(e)}
-            value={formData.Region}
-            className="form__text_field"
-            id="Region"
-            name="Region"
-            variant="filled"
-            label="Region"
-          />
-          <TextField
-            onChange={(e) => handleChange(e)}
-            value={formData.TrafficType}
-            className="form__text_field"
-            id="TrafficType"
-            name="TrafficType"
-            variant="filled"
-            label="TrafficType"
-          />
-          <TextField
-            onChange={(e) => handleChange(e)}
-            value={formData.VisitorType}
-            className="form__text_field"
-            id="VisitorType"
-            name="VisitorType"
-            variant="filled"
-            label="Visitor type; 0, 1, or 2"
-          />
+            <TextField
+              onChange={(e) => handleChange(e)}
+              value={formData.BounceRates}
+              className="form__text_field"
+              id="BounceRates"
+              name="BounceRates"
+              variant="filled"
+              label="BounceRates(float)"
+            />
+            <TextField
+              onChange={(e) => handleChange(e)}
+              value={formData.ExitRates}
+              className="form__text_field"
+              id="ExitRates"
+              name="ExitRates"
+              variant="filled"
+              label="ExitRates(float)"
+            />
+            <TextField
+              onChange={(e) => handleChange(e)}
+              value={formData.PageValues}
+              className="form__text_field"
+              id="PageValues"
+              name="PageValues"
+              variant="filled"
+              label="PageValues (float)"
+            />
+            <TextField
+              onChange={(e) => handleChange(e)}
+              value={formData.SpecialDay}
+              className="form__text_field"
+              id="SpecialDay"
+              name="SpecialDay"
+              variant="filled"
+              label="SpecialDay (float)"
+            />
+            <TextField
+              onChange={(e) => handleChange(e)}
+              value={formData.Month}
+              className="form__text_field"
+              id="Month"
+              name="Month"
+              variant="filled"
+              label="Month in numbers starting from 0"
+            />
+            <TextField
+              onChange={(e) => handleChange(e)}
+              value={formData.OperatingSystems}
+              className="form__text_field"
+              id="OperatingSystems"
+              name="OperatingSystems"
+              variant="filled"
+              label="OperatingSystems"
+            />
+            <TextField
+              onChange={(e) => handleChange(e)}
+              value={formData.Region}
+              className="form__text_field"
+              id="Region"
+              name="Region"
+              variant="filled"
+              label="Region"
+            />
+            <TextField
+              onChange={(e) => handleChange(e)}
+              value={formData.TrafficType}
+              className="form__text_field"
+              id="TrafficType"
+              name="TrafficType"
+              variant="filled"
+              label="TrafficType"
+            />
+            <TextField
+              onChange={(e) => handleChange(e)}
+              value={formData.VisitorType}
+              className="form__text_field"
+              id="VisitorType"
+              name="VisitorType"
+              variant="filled"
+              label="Visitor type; 0, 1, or 2"
+            />
 
-          <Button
-            onClick={() => handleClick()}
-            className="form__button"
-            color="primary"
-            variant="contained"
-          >
-            Predict intent
-          </Button>
+            <Button
+              onClick={() => handleClick()}
+              className="form__button"
+              color="primary"
+              variant="contained"
+            >
+              Predict intent
+            </Button>
+          </div>
         </div>
-      </div>
+      </>
     );
 }
 
